adeb88135d3e2f2e0a2c9079bc293acc13f9d586
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-10-16 16:43:39 -0600

    passing all tests in Postman and Rails up to 'Get One Item'

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -47,4 +47,39 @@ describe "Items API" do
     items = JSON.parse(response.body, symbolize_names: true)
     expect(items[:data].count).to eq 20
   end
+
+  it 'can get a specific item' do
+    merchant_1 = create(:merchant)
+    merchant_2 = create(:merchant)
+    items_1 = create_list(:item, 10, merchant: merchant_1)
+    items_2 = create_list(:item, 10, merchant: merchant_2)
+    item = Item.first
+
+    get "/api/v1/items/#{item.id}"
+
+    expect(response).to be_successful
+
+    item_returned = JSON.parse(response.body, symbolize_names: true)
+
+    expect(item_returned.count).to eq 1
+
+    expect(item_returned[:data]).to have_key(:id)
+    expect(item_returned[:data][:id]).to eq(item.id.to_s)
+
+    expect(item_returned[:data]).to have_key(:type)
+    expect(item_returned[:data][:type]).to eq('item')
+
+    expect(item_returned[:data]).to have_key(:attributes)
+    expect(item_returned[:data][:attributes]).to have_key(:name)
+    expect(item_returned[:data][:attributes][:name]).to be_a(String)
+
+    expect(item_returned[:data][:attributes]).to have_key(:description)
+    expect(item_returned[:data][:attributes][:description]).to be_a(String)
+
+    expect(item_returned[:data][:attributes]).to have_key(:unit_price)
+    expect(item_returned[:data][:attributes][:unit_price]).to be_a(Float)
+
+    expect(item_returned[:data][:attributes]).to have_key(:merchant_id)
+    expect(item_returned[:data][:attributes][:merchant_id]).to be_an(Integer)
+  end
 end
