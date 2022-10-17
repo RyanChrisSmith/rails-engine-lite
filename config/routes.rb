c86f100e770eeb251719219b650b691605f6fa3e
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-10-17 06:20:24 -0600

    create item tested

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      resources :items, only: [:index, :show, :update, :create]
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index], controller: :merchant_items
       end
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
-      resources :items, only: [:index, :show]
     end
   end
 
