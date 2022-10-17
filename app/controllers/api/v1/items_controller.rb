cca7e1d9f6d2b8d08bf8fd05125a5cfbc1cd356b
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-10-17 06:59:49 -0600

    postman tested through part 1a and 1b

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -16,7 +16,12 @@ class Api::V1::ItemsController < ApplicationController
   end
 
   def update
-    render json: ItemSerializer.new(Item.update(params[:id], item_params))
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      render json: ItemSerializer.new(item), status: :ok
+    else
+      render status: 404
+    end
   end
 
   private
