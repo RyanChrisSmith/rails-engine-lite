15716aec71cfbf66eafc7fb4ed0ae9bd0a1ea847
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-10-17 19:28:55 -0600

    find all merchants by partial name tested, and error path shown as well

--- app/controllers/api/v1/items_merchant_controller.rb
+++ app/controllers/api/v1/items_merchant_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::ItemsMerchantController < ApplicationController
-  def index 
-    item = Item.find(params[:item_id])
-    render json: MerchantSerializer.new(Merchant.find(item.merchant_id))
+  def index
+    if item = Item.find(params[:item_id])
+      render json: MerchantSerializer.new(Merchant.find(item.merchant_id))
+    else
+      render status: 404
+    end
   end
 end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -24,6 +24,16 @@ describe "Merchants API" do
     end
   end
 
+  it 'SAD PATH will return a blank parse if no merchants are in the database' do
+    get api_v1_merchants_path
+
+    expect(response).to be_successful
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants[:data].count).to eq 0
+    expect(merchants[:data]).to eq([])
+  end
+
   it 'can get one merchant by its id' do
     id = create(:merchant).id
 
