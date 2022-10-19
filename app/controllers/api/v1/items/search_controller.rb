913b224bcaff1a9c11a0879f26c0a1c5b5c3f9da
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-10-19 15:11:23 -0600

    fixing some routing to make tests work everywhere

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::Items::SearchController < ApplicationController
   before_action :check_for_faulty_params
 
   def find_one
-    if params[:name].present?
+    if params[:name]
       render json: ItemSerializer.new(Item.search(params[:name]))
     elsif params[:min_price] != nil || params[:max_price] != nil
       render json: ItemSerializer.new(Item.price_search(params).first)
@@ -12,7 +12,9 @@ class Api::V1::Items::SearchController < ApplicationController
 
   private
   def check_for_faulty_params
-    if params[:name].present? && params[:min_price].present? || params[:max_price].present? && params[:name].present?
+    if (params[:name]) == "" || params[:min_price] == "" || params[:max_price] == ""
+      render status: 400
+    elsif params[:name].present? && params[:min_price].present? || params[:max_price].present? && params[:name].present?
       render json: {errors: { details: "Must search by name OR price"}}, status: 400
     elsif (params[:min_price]).to_i < 0 || (params[:max_price]).to_i < 0
       render json: {errors: { details: "Price search cannot use a negative number"}}, status: 400
